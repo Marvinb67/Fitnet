26eb0f981782108fb10f3c7d43c5a07de48fd1bf
ama1800 <ama1800@hotmail.fr>
2023-02-07 16:56:31 +0100

    amelioration du systéme de likes

--- assets/app.js
+++ assets/app.js
@@ -15,6 +15,6 @@ import './styles/layout/mobile/desktop/_tchat.scss';
 
 import './bootstrap';
 import './js/datepicker';
-import './js/likes';
+// import './js/likes';
 
 
--- assets/js/likes.js
+++ assets/js/likes.js
@@ -4,68 +4,85 @@ import { setTimeout } from "core-js";
 /**
  * Gestion du systéme du like/dislike par ajax
  */
-
 // selection des icons like/dislike
 let thumbs = document.querySelectorAll(".reactions");
-
-// Fournis les stats des likes/dislikes
-axios.get("reaction/stats").then((response) => {
-  let data = response.data.likes;
-  for (let stat of data) {
-    let idPublication = stat.idPublication;
-    let countLikes = stat.countLikes;
-    let countDisLikes = stat.countDisLikes;
-    const likesSpan = document.getElementById(`j-aime-${idPublication}`);
-    const dislikesSpan = document.getElementById(`j-aimePas-${idPublication}`);
-    likesSpan.innerHTML = countLikes;
-    dislikesSpan.innerHTML = countDisLikes;
-  }
-});
-
-// On boucle sur toutes les icons
-Array.from(thumbs, (thumb) => {
-  // Ecouteur d'evenement sur l'icon cloquer
-  thumb.addEventListener("click", () => {
-    // l'icon cliquer
-    let aimer = thumb.classList.contains("fa-thumbs-up") ? true : false;
-    // Class à ajouter à l'icon
-    let class1 = aimer ? "j-aime" : "j-aimePas";
-    // Récupération de l'Id de la publication
-    let postId = thumb.attributes["data-id"].value;
-    // Compte en temps reel des likes/DisLikes
-    const countSpanLikes = document.getElementById(`j-aime-${postId}`);
-    const countSpanDisLikes = document.getElementById(`j-aimePas-${postId}`);
-    // Génére l'Url like/dislike
-    const url = `reaction/like/${postId}-${aimer}`;
-    // Ajax avec Axios
-    axios.get(url).then((response) => {
-      if (response.data == 502) {
-        //Redirection vers la page de connexion si non connécter
-        return (window.location = "/connexion");
-      }
-      // Récupération des données envoyer par le controller aprés le traitement
-      let message = response.data.message;
-      let reaction = response.data.reaction;
-      let countLikes = response.data.countLikes;
-      let countDisLikes = response.data.countDisLikes;
-      // Au cas d'une réaction on rafraichi les donnees afficher
-      if (reaction == 1) {
-        thumb.classList.toggle(class1);
-        countSpanLikes.innerHTML = countLikes;
-        countSpanDisLikes.innerHTML = countDisLikes;
-      }
-      // Au cas d'annulation du like/dislike on rafraichi les donnees afficher
-      else {
-        class1 === "j-aimePas"
-          ? popup(thumb, message, 80)
-          : popup(thumb, message, 112);
-        thumb.classList.remove(class1);
-        countSpanLikes.innerHTML = countLikes;
-        countSpanDisLikes.innerHTML = countDisLikes;
-      }
+  // On boucle sur toutes les icons
+  Array.from(thumbs, (thumb) => {
+    // Ecouteur d'evenement sur l'icon cloquer
+    thumb.addEventListener("click", () => {
+      // l'icon cliquer
+      let aimer = thumb.classList.contains("fa-thumbs-up") ? true : false;
+      // Class à ajouter à l'icon
+      let class1 = aimer ? "j-aime" : "j-aimePas";
+      // Récupération de l'Id de la publication
+      let postId = thumb.attributes["data-id"].value;
+      // Compte en temps reel des likes/DisLikes
+      const countSpanLikes = document.getElementById(`j-aime-${postId}`);
+      const countSpanDisLikes = document.getElementById(`j-aimePas-${postId}`);
+      // Génére l'Url like/dislike
+      const url = `reaction/like/${postId}-${aimer}`;
+      // Ajax avec Axios
+      axios
+        .get(url)
+        .then((response) => {
+          if (response.data == 502) {
+            //Redirection vers la page de connexion si non connécter
+            return (window.location = "/connexion");
+          }
+          // Récupération des données envoyer par le controller aprés le traitement
+          let message = response.data.message;
+          let reaction = response.data.reaction;
+          let countLikes = response.data.countLikes;
+          let countDisLikes = response.data.countDisLikes;
+          // Au cas d'une réaction on rafraichi les donnees afficher
+          if (reaction == 1) {
+            thumb.classList.toggle(class1);
+            countSpanLikes.innerHTML = countLikes;
+            countSpanDisLikes.innerHTML = countDisLikes;
+          }
+          // Au cas d'annulation du like/dislike on rafraichi les donnees afficher
+          else {
+            class1 === "j-aimePas"
+              ? popup(thumb, message, 80)
+              : popup(thumb, message, 112);
+            thumb.classList.remove(class1);
+            countSpanLikes.innerHTML = countLikes;
+            countSpanDisLikes.innerHTML = countDisLikes;
+          }
+        })
+        .catch((err) => {
+          if (err) {
+            // ...
+          }
+        });
     });
   });
-});
+
+  // Fournis les stats des likes/dislikes
+  let postId = document.querySelector('.reactions').attributes["data-id"].value;
+  const lien = (window.location.pathname === '/publication' || window.location.pathname === '/') ? `reaction/stats` : `reaction/stats/${postId}`;
+  axios
+    .get(lien)
+    .then((response) => {
+      let data = response.data.likes;
+  console.log(data);
+      for (let stat of data) {
+        let idPublication = stat.idPublication;
+        let countLikes = stat.countLikes;
+        let countDisLikes = stat.countDisLikes;
+        const likesSpan = document.getElementById(`j-aime-${idPublication}`);
+        const dislikesSpan = document.getElementById(
+          `j-aimePas-${idPublication}`
+        );
+        likesSpan.innerHTML = countLikes;
+        dislikesSpan.innerHTML = countDisLikes;
+      }
+    })
+    .catch((err) => {
+      if (err) {
+        // ...
+      }
+    });
 // Fonction qui gére le petit popup au cas d'annulation du like/dislike
 const popup = (element, message, left) => {
   let pop = element.parentElement.parentElement.children[0].children[0];
